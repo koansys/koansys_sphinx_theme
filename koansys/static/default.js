293e6c4b155632e007d1decc078c8322f5295f22
Reed O'Brien <reed@koansys.com>
2012-10-23 12:14:32 -0400

    remove the alerts

--- koansys/static/default.js
+++ koansys/static/default.js
@@ -1,11 +1,5 @@
 $(function(){
-	alert("herro");
-	$("#navigationWrapper #navigation li").each(function() {
-		var accessKey = $(this).attr("accesskey");
-		alert($(this));
-		
 		if (accessKey == "N" || accessKey == "P") {
 			$(this).addClass("prevNext");
 		}
 	});
-});
