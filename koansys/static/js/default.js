be9df93cecaffd73fc2ec2258acac0ca797810b3
Mckenzie Tully <mckenzie@koansys.com>
2013-01-21 23:18:47 -0500

    Koansys Sphinx Theme Responsive Dev
    [#42158143]
    -Updating the Koansys sphinx theme to use responsive elements from twitter's bootstrap
    -General modifications and overhauls to layout and design given new data from koansys default documentation

--- koansys/static/js/default.js
+++ koansys/static/js/default.js
@@ -2,4 +2,18 @@ jQuery(function(){
 	jQuery("a.headerlink").each(function() {
 		jQuery(this).html("&dArr;");
 	});
+	
+	jQuery(".section blockquote div").each(function() {
+		if(jQuery(this).is(":empty")) {
+			jQuery(this).closest(".section").remove();
+		}
+	});
+	
+	jQuery(".section").each(function(){
+		if(jQuery(this).add("#functions").length > 0) {
+			console.log("hello");
+			jQuery(this).addClass("functionsWrapper");
+		}
+	});
+		
 });
