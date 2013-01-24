86d1c39e4f8c4c56fd35701c7f4ea0f6283bab4c
Mckenzie Tully <mckenzie@koansys.com>
2013-01-24 12:53:06 -0500

    Koansys Sphinx Theme Responsive Dev
    [#42158143]
    -Updating the Koansys sphinx theme to use responsive elements from twitter's bootstrap
    -General modifications and overhauls to layout and design given new data from koansys default documentation

--- koansys/static/js/default.js
+++ koansys/static/js/default.js
@@ -11,9 +11,35 @@ jQuery(function(){
 	
 	jQuery(".section").each(function(){
 		if(jQuery(this).add("#functions").length > 0) {
-			console.log("hello");
 			jQuery(this).addClass("functionsWrapper");
 		}
+		
+		var sectionID = jQuery(this).attr("id");
+		jQuery(this).replaceWith(jQuery('<section id=' + sectionID + '/>').html(jQuery(this).html()));
 	});
+	
+	//Add current page link to sidebar navigation
+	var firstLink = jQuery(".sidebarNav li.active a").first()
+	var linkTxt = firstLink.text().toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
+	var newLink = "#" + linkTxt;
+	
+	firstLink.attr("href", newLink);
+	
+	jQuery(".sidebarNav ul li").each(function() {
+		jQuery(this).attr("class", "");
+	});
+	
+	jQuery(".sidebarNav ul li").each(function(index) {
+		if(jQuery(this).children("ul").length > 0) {
+			jQuery(this).addClass("hasChildren");
+		}
+	});
+		
+	var $window = $(window);
+    jQuery('.sidebarNav').affix({
+		offset: {
+			top: function () { return $window.width() <= 980 ? 290 : 210 }
+		}
+	})
 		
 });
